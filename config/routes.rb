cc76391c3c65f68bf98a1543845748a28a8bdf13
Nathan Tornquist <ntornquist@enova.com>
2015-07-24 13:49:03 -0500

    Introduced Users controller and created basic user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  get '/users/:id', to: 'users#show', as: "user_profile"
   get '/', to: 'static#index'
 end

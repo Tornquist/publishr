308c4e4f2a357bab40b4f3c1aac3bb46850922d8
Nathan Tornquist <ntornquist@enova.com>
2015-07-27 12:17:50 -0500

    User factory, features, and step definition support

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   get '/users/:id', to: 'users#show', as: "user_profile"
-  get '/', to: 'static#index'
+  get '/', to: 'static#index', as: "home"
 end
